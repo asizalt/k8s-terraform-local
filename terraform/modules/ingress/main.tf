001cb497706bd4af5f62a69c08c3a7131a89a72a
asizalt <asi.zaltzman@gmail.com>
2026-04-07 19:24:47 +0300

    Add ingress module: nginx ingress controller via Helm

---
+++ terraform/modules/ingress/main.tf
@@ -0,0 +1,31 @@
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = var.chart_version
+  namespace        = var.namespace
+  create_namespace = true
+
+  # Required for kind: bind directly to the node's host ports
+  set {
+    name  = "controller.hostPort.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.service.type"
+    value = "NodePort"
+  }
+
+  # Only schedule on nodes labelled ingress-ready=true
+  # type = "string" prevents Helm from coercing "true" to a boolean
+  set {
+    name  = "controller.nodeSelector.ingress-ready"
+    value = "true"
+    type  = "string"
+  }
+
+  # Wait until the controller is fully ready before Terraform proceeds
+  wait    = true
+  timeout = 300
+}
